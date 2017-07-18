6e7bf04b00c9f14f451d4b7e24c25fd6845dc3eb
Carlos Maniero <cmaniero@thoughtworks.com>
2017-07-17 22:22:55 -0300

    Add register of many transaction
    The RegisterMany method works async and import a lot of transaction
    simultaneously.

--- usecases/transaction.go
+++ usecases/transaction.go
@@ -1,10 +1,22 @@
 package usecases
 
-import "github.com/carlosmaniero/budgetgo-ofximporter/domain"
+import (
+	"github.com/carlosmaniero/budgetgo-ofximporter/domain"
+)
 
 // TransactionIterator contains all transactions usecases
 type TransactionIterator struct {
-	Repository TransactionRepository
+	Repository       TransactionRepository
+	FundingID        string
+	ConcurrencyLevel int
+}
+
+func (iterator *TransactionIterator) getConcurrentyLevel() int {
+	if iterator.ConcurrencyLevel == 0 {
+		return 100
+	}
+
+	return iterator.ConcurrencyLevel
 }
 
 // Register one transaction inside the repository
@@ -12,7 +24,60 @@ func (iterator *TransactionIterator) Register(transaction *domain.Transaction) e
 	return iterator.Repository.Store(transaction)
 }
 
+func (iterator *TransactionIterator) registerAsync(total int, transactions TransactionList, sent chan bool) {
+	for i := 0; i < total; i++ {
+		go func() {
+			transaction := domain.Transaction{}
+
+			if err := transactions.Next(&transaction); err == nil {
+				transaction.FundingID = iterator.FundingID
+				iterator.Register(&transaction)
+			}
+
+			sent <- true
+		}()
+	}
+}
+
+func (iterator *TransactionIterator) waitForTransactions(total int, sent chan bool) {
+	totalSented := 0
+	for {
+		<-sent
+		totalSented++
+
+		if totalSented == total {
+			return
+		}
+	}
+}
+
+// RegisterMany will register many transactions
+func (iterator *TransactionIterator) RegisterMany(list TransactionList) {
+	sent := make(chan bool, iterator.getConcurrentyLevel())
+	totalSent := 0
+
+	for list.HasNext() {
+		toSend := iterator.getConcurrentyLevel()
+
+		if toSend > list.Remaining() {
+			toSend = list.Remaining()
+		}
+
+		iterator.registerAsync(toSend, list, sent)
+		iterator.waitForTransactions(toSend, sent)
+		totalSent += iterator.getConcurrentyLevel()
+	}
+}
+
 // TransactionRepository is the place where transactions are stored
 type TransactionRepository interface {
 	Store(*domain.Transaction) error
+}
+
+// TransactionList is a dynamic list of transactions
+type TransactionList interface {
+	HasNext() bool
+	Next(*domain.Transaction) error
+	Count() int
+	Remaining() int
 }
--- usecases/transaction_test.go
+++ usecases/transaction_test.go
@@ -1,6 +1,8 @@
 package usecases
 
 import (
+	"errors"
+	"sync"
 	"testing"
 	"time"
 
@@ -32,6 +34,81 @@ func TestSpecCreateTransaction(t *testing.T) {
 			})
 		})
 	})
+	Convey("Scenario: Creating many transaction", t, func() {
+		Convey("Given I've a valid transaction List", func() {
+			transaction := domain.Transaction{
+				Description: "Golpher Shampoo",
+				Amount:      -10,
+				Date:        time.Now(),
+				FundingID:   "pet-account",
+			}
+
+			items := make([]*domain.Transaction, 0)
+
+			for i := 0; i < 123; i++ {
+				items = append(items, &transaction)
+			}
+
+			transactionList := SimpleTransactionList{
+				items: items,
+			}
+
+			Convey("When I register many", func() {
+				repository := mockTransactionRepository{count: 0}
+				iterator := TransactionIterator{
+					Repository: &repository,
+				}
+				iterator.RegisterMany(&transactionList)
+
+				Convey("Then all transactions was got", func() {
+					So(transactionList.Remaining(), ShouldEqual, 0)
+				})
+
+				Convey("And all transactions was registred", func() {
+					So(repository.count, ShouldEqual, 123)
+				})
+			})
+		})
+	})
+}
+
+type SimpleTransactionList struct {
+	items   []*domain.Transaction
+	current int
+	mux     sync.Mutex
+}
+
+// Next returns the next transaction
+func (iterator *SimpleTransactionList) Next(transaction *domain.Transaction) error {
+	iterator.mux.Lock()
+	if !iterator.HasNext() {
+		iterator.mux.Unlock()
+		return errors.New("No more transactions")
+	}
+
+	transaction = iterator.items[iterator.current]
+	iterator.current++
+	iterator.mux.Unlock()
+
+	return nil
+}
+
+// HasNext checks if has next
+func (iterator *SimpleTransactionList) HasNext() bool {
+	return iterator.current < len(iterator.items)
+}
+
+// Count returns the total of transactions
+func (iterator *SimpleTransactionList) Count() int {
+	return len(iterator.items)
+}
+
+// Remaining returns the total of transactions
+func (iterator *SimpleTransactionList) Remaining() int {
+	iterator.mux.Lock()
+	remaining := iterator.Count() - iterator.current
+	iterator.mux.Unlock()
+	return remaining
 }
 
 type mockTransactionRepository struct {
